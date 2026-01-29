6eb2067712ac7e2379d04ac4710fc3967ba67135
Andre Espinoza <ae95caba@gmail.com>
2026-01-28 23:53:07 -0300

    add the flavours count on the menu list

--- src/Components/List.tsx
+++ src/Components/List.tsx
@@ -1,11 +1,12 @@
 import React from "react";
 
 export default function List({ items }) {
+  console.log(items);
   return (
     <ul>
       {items.map((item) => (
         <li key={item._id}>
-          <span>{item.name}</span> <span>${item.price}</span>
+          <span>{item.name} {item.flavours ? `(${item.flavours} sabores)` : ""} </span> <span>${item.price}</span>
         </li>
       ))}
     </ul>
